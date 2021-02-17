c44f7ea83ff8d6eeec8eede4f62bd9df6e70ceb9
emoueitchaien <manojpradhan628@gmail.com>
2021-02-17 23:50:37 +0545

    sample database upload

--- wt_crud.sql
+++ wt_crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2021 at 10:05 AM
+-- Generation Time: Feb 17, 2021 at 07:02 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `wt_crud`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `username` varchar(8) NOT NULL,
+  `password1` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id`, `name`, `email`, `username`, `password1`) VALUES
+(21, 'Ram Thapa', 'ramthapa@gmail.com', 'ram', 'ram123'),
+(22, 'Hari Pokharel', 'pokharelhari@gmail.com', 'hari', 'hari123');
 
 -- --------------------------------------------------------
 
@@ -44,12 +66,18 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `roll_no`, `faculty`, `batch`, `dob`, `address`, `email`, `phone`) VALUES
-(8, 'Mohan Pradhan', 101, 'B.Sc. CSIT', 2074, '2000-01-21', 'Arghakhanchi, Nepal', 'manojpradhan628@gmail.com', '9867238425'),
-(10, 'Hemsagar Adhikari', 139, 'B.Sc. CSIT', 2074, '1997-12-17', 'Biratnagar, Nepal', 'hemsagar071@gmail.com', '9842484002');
+(8, 'Mohan Pradhan', 103, 'B.Sc. CSIT', 2074, '2000-01-21', 'Arghakhanchi, Nepal', 'manojpradhan628@gmail.com', '9867238425'),
+(19, 'Hemsagar Adhikari', 139, 'B.Sc. CSIT', 2074, '1997-12-17', 'Nepal', 'hemsagar071@gmail.com', '9842484002');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `students`
@@ -62,10 +90,16 @@ ALTER TABLE `students`
 --
 
 --
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
